7c6e18d47c7030a422f2aeab91a4eadced75c980
以恒 陈 <RogerChen200506@outlook.com>
2022-02-02 20:14:29 +0800

    change

--- tools/Checker/main.js
+++ tools/Checker/main.js
@@ -1,7 +1,33 @@
-var list = ["cover.html","page1.html","page2.html","page3.html","about.html"];
+var list = [
+    "home.html",
+    "check.html",
+    "history.html",
+    "more.html",
+    "about.html",
+    "checked.html"];
+var hasChecked = false;
+
+function getNowFormatDate(){
+           var day = new Date();
+           var Year = day.getFullYear();
+           var Month = day.getMonth()+1;
+           var Day = day.getDate();
+           var CurrentDate = "";
+           CurrentDate += Year + "/";
+           Month >= 10?CurrentDate += Month + "/":CurrentDate += "0" + Month + "/";
+           Day >= 10?CurrentDate += Day:CurrentDate += "0" + Day;
+           return CurrentDate;
+}
+var curTime = getNowFormatDate();
+var dailyTodo = localStorage.getItem("toDo");
+if(!dailyTodo == null){
+    hasChecked = true;
+}
 
 function change(index,t){
-    $("#UI").animate({opacity:"0"},250,"swing",function(){document.getElementById("UI").src = "subsite/"+list[index]})
+    $("#UI").animate({opacity:"0"},250,"swing",function(){
+        document.getElementById("UI").src = "subsite/"+list[index]
+    });
     $("#UI").animate({opacity:"1"},250,"swing");
     $(".cur_display").removeClass("cur_display");
     $(t).addClass("cur_display");
